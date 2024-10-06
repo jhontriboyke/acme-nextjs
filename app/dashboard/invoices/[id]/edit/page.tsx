fc44b403c82f212bc0665f1891d4b63b324c88d1
Jhontri Boyke <51961172+jhontriboyke@users.noreply.github.com>
2024-10-06 23:37:46 +0700

    fix(): dashboard/invoices/[id]/edit/page.tsx param handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 };
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const id = await params.id;
+  const id = params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
